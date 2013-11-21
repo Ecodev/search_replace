fa97c0233effcd49aa17d5ee1ae6cbba3c68a4aa
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-11-21 09:48:29 +0100

    [BUGFIX] Add forgotten pid field

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,7 @@
 #
 CREATE TABLE tx_ecopatterns (
 	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
